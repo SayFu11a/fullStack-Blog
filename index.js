8a4ee218147f63441e69acb8d97617d4fdf66492
SayFu11a <sgishlakaev@mail.ru>
2024-02-16 15:13:37 +0300

    finish backEnd allmost

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ import multer from 'multer';
 // import { restart } from 'nodemon';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations/auth.js';
-import checkAuth from './utils/checkAuth.js';
-import * as UserController from './controllers/UserController.js'; // все методы из UserController.js сохрани в перемен UserController
-import * as PostController from './controllers/PostController.js';
+
+import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { UserController, PostController } from './controllers/index.js'; // все методы из UserController.js сохрани в перемен UserController
 
 // import User from './models/User.js';
 
@@ -29,19 +29,32 @@ const storage = multer.diskStorage({
    },
 });
 
-const upload = multer({ storage }); // у мультера храниище сделали стородж
+const upload = multer({ storage }); // у мультера храниище сделалитз стородж
 
 app.use(express.json()); // чтобы читать джейсон запросы.
+app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register); //  если придет пост запрос вернуть сексес тру, регистрация
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register); //  если придет пост запрос вернуть сексес тру, регистрация
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+   res.json({
+      url: `/uploads/${req.file.originalname}`,
+   });
+});
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch(
+   '/posts/:id',
+   checkAuth,
+   postCreateValidation,
+   handleValidationErrors,
+   PostController.update,
+);
 
 app.listen(4444, (err) => {
    if (err) {
